df1692f09d25a36656aa5d067cb6173ccc0b9cd5
Anders Njøs Slinde <anders.slinde@gmail.com>
2023-11-10 12:03:25 +0100

    asdf

--- rules.mk
+++ rules.mk
@@ -3,6 +3,6 @@ RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = yes    # Breathing sleep LED during USB suspend
-OLED_ENABLE     = no
+OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
